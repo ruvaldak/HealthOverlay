31652dea901af264a0b392085870988252bd2393
Terrails <terrails@outlook.com>
2019-04-04 01:18:37 +0200

    Rewritten the code and updated to 19w14a

---
+++ src/main/java/terrails/healthoverlay/HealthRenderer.java
@@ -0,0 +1,179 @@
+package terrails.healthoverlay;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.SystemUtil;
+import net.minecraft.util.math.MathHelper;
+import terrails.healthoverlay.api.HealthRendererConfiguration;
+
+import java.util.Random;
+
+@Environment(EnvType.CLIENT)
+public class HealthRenderer {
+
+    public static HealthRenderer INSTANCE = new HealthRenderer();
+
+    private static final Identifier HEART_ICONS_LOCATION = new Identifier("healthoverlay:textures/hearts.png");
+
+    private MinecraftClient client = MinecraftClient.getInstance();
+    private InGameHud hud = client.inGameHud;
+    private Random random = new Random();
+    private PlayerEntity player;
+
+    private int prevHealth, health;
+    private long prevSystemTime, healthTicks;
+
+    /**
+     * Copied from InGameHud, still need to find
+     * some reasonable names for the variables
+     */
+    public void render(PlayerEntity player) {
+        this.player = player;
+        int ticks = hud.getTicks();
+
+        int int_1 = MathHelper.ceil(player.getHealth());
+        boolean boolean_1 = this.healthTicks > (long) ticks && (this.healthTicks - (long) ticks) / 3L % 2L == 1L;
+        long long_1 = SystemUtil.getMeasuringTimeMs();
+        if (int_1 < this.health && player.field_6008 > 0) {
+            this.prevSystemTime = long_1;
+            this.healthTicks = (long)(ticks + 20);
+        } else if (int_1 > this.health && player.field_6008 > 0) {
+            this.prevSystemTime = long_1;
+            this.healthTicks = (long)(ticks + 10);
+        }
+
+        if (long_1 - this.prevSystemTime > 1000L) {
+            this.health = int_1;
+            this.prevHealth = int_1;
+            this.prevSystemTime = long_1;
+        }
+
+        this.health = int_1;
+        int int_2 = this.prevHealth;
+        random.setSeed((long)(ticks * 312871));
+        int int_4 = this.client.window.getScaledWidth() / 2 - 91;
+        int int_6 = this.client.window.getScaledHeight() - 39;
+        float float_1 = Math.min(20, player.getHealthMaximum());
+        int int_7 = Math.min(20, MathHelper.ceil(player.getAbsorptionAmount()));
+        int int_8 = MathHelper.ceil((float_1 + (float)int_7) / 2.0F / 10.0F);
+        int int_9 = Math.max(10 - (int_8 - 2), 3);
+        int int_12 = int_7;
+        int int_14 = -1;
+        if (player.hasPotionEffect(StatusEffects.REGENERATION)) {
+            int_14 = ticks % MathHelper.ceil(float_1 + 5.0F);
+        }
+
+        for(int int_17 = MathHelper.ceil((float_1 + (float)int_7) / 2.0F) - 1; int_17 >= 0; --int_17) {
+            int int_18 = 16;
+            if (player.hasPotionEffect(StatusEffects.POISON)) {
+                int_18 += 36;
+            } else if (player.hasPotionEffect(StatusEffects.WITHER)) {
+                int_18 += 72;
+            }
+
+            int int_19 = 0;
+            if (boolean_1) {
+                int_19 = 1;
+            }
+
+            int int_26 = MathHelper.ceil((float)(int_17 + 1) / 10.0F) - 1;
+            int int_21 = int_4 + int_17 % 10 * 8;
+            int int_22 = int_6 - int_26 * int_9;
+            if (int_1 <= 4) {
+                int_22 += random.nextInt(2);
+            }
+
+            if (int_12 <= 0 && int_17 == int_14) {
+                int_22 -= 2;
+            }
+
+            int int_23 = 0;
+            if (player.world.getLevelProperties().isHardcore()) {
+                int_23 = 5;
+            }
+
+            // Modified to make the ghost hearts appear
+            this.client.getTextureManager().bindTexture(HEART_ICONS_LOCATION);
+            for (int j = MathHelper.ceil((20.0F - float_1) / 2.0F) - 1; j >= 0 && int_17 == 0 && HealthRendererConfiguration.GHOST_HEARTS; --j) {
+                int jx = int_4 + (MathHelper.ceil(float_1 / 2.0f) + j) % 10 * 8;
+                hud.blit(jx, int_6 - (int_12 > 0 ? 10 : 0), 0, 0, 9, 9);
+            }
+            this.client.getTextureManager().bindTexture(DrawableHelper.GUI_ICONS_LOCATION);
+
+            if (int_17 * 2 + 1 > float_1 - 1 || (int_12 == int_7 && int_7 % 2 == 1)) {
+                this.client.getTextureManager().bindTexture(HEART_ICONS_LOCATION);
+                hud.blit(int_21, int_22, 9 + int_19 * 9, 0, 9, 9);
+                this.client.getTextureManager().bindTexture(DrawableHelper.GUI_ICONS_LOCATION);
+            } else {
+                hud.blit(int_21, int_22, 16 + int_19 * 9, 9 * int_23, 9, 9);
+            }
+
+
+            if (boolean_1) {
+                if (int_17 * 2 + 1 < int_2) {
+                    hud.blit(int_21, int_22, int_18 + 54, 9 * int_23, 9, 9);
+                }
+
+                if (int_17 * 2 + 1 == int_2) {
+                    hud.blit(int_21, int_22, int_18 + 63, 9 * int_23, 9, 9);
+                }
+            }
+
+            if (int_12 > 0) {
+                if (int_12 == int_7 && int_7 % 2 == 1) {
+                    hud.blit(int_21, int_22, int_18 + 153, 9 * int_23, 9, 9);
+                    --int_12;
+                } else {
+                    hud.blit(int_21, int_22, int_18 + 144, 9 * int_23, 9, 9);
+                    int_12 -= 2;
+                }
+            } else {
+                if (int_17 * 2 + 1 < int_1) {
+                    hud.blit(int_21, int_22, int_18 + 36, 9 * int_23, 9, 9);
+                }
+
+                if (int_17 * 2 + 1 == int_1) {
+                    hud.blit(int_21, int_22, int_18 + 45, 9 * int_23, 9, 9);
+                }
+            }
+        }
+
+        this.renderHearts(int_4, int_6, int_14, false);
+        this.renderHearts(int_4, int_6 - 10, int_14, true);
+    }
+
+    private void renderHearts(int int_4, int int_6, int int_14, boolean absorption) {
+        if (this.player.hasPotionEffect(StatusEffects.WITHER)) {
+            return;
+        }
+        int y = absorption ? 75 : 10;
+        if (this.player.hasPotionEffect(StatusEffects.POISON)) {
+            y += 9;
+        }
+
+        if (this.player.world.getLevelProperties().isHardcore()) {
+            y += 27;
+        }
+
+        int currentValue = MathHelper.ceil(absorption ? this.player.getAbsorptionAmount() : this.player.getHealth()) - 20;
+        this.client.getTextureManager().bindTexture(HEART_ICONS_LOCATION);
+        for (int i = 0; i < MathHelper.ceil(currentValue / 2.0F); ++i) {
+            int value = i * 2 + 1;
+            int regenOffset = !absorption && (i - (10 * (i / 10))) == int_14 ? -2 : 0;
+            int typeOffset = (value / 20) % 12;
+
+            if (value == currentValue) {
+                hud.blit(int_4 + i % 10 * 8, int_6 + regenOffset, 9 + 18 * typeOffset, y, 9, 9);
+            } else if (absorption || value < currentValue) {
+                hud.blit(int_4 + i % 10 * 8, int_6 + regenOffset, 18 * typeOffset, y, 9, 9);
+            }
+        }
+        this.client.getTextureManager().bindTexture(DrawableHelper.GUI_ICONS_LOCATION);
+    }
+}
--- src/main/java/terrails/healthoverlay/mixin/InGameHudMixin.java
+++ src/main/java/terrails/healthoverlay/mixin/InGameHudMixin.java
@@ -1,184 +1,35 @@
 package terrails.healthoverlay.mixin;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.Drawable;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-import terrails.healthoverlay.HealthOverlay;
-
-import java.util.Random;
+import terrails.healthoverlay.HealthRenderer;
 
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
 
-    private static final Identifier HEART_TEXTURES = new Identifier(HealthOverlay.MOD_ID, "textures/hearts.png");
-
-    @Shadow private @Final MinecraftClient client;
-    @Shadow private int ticks;
-    @Shadow private int scaledWidth;
-    @Shadow private int scaledHeight;
-
-    @Shadow private @Final Random random;
-    @Shadow private long field_2032; // healthTicks
-
-    @Redirect(method = "renderStatusBars",
-            slice = @Slice(from = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V", args = "ldc=health")),
+    @Redirect(method = "renderStatusBars", slice = @Slice(from = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V", args = "ldc=health")),
             at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;ceil(F)I", ordinal = 0))
     private int runDefaultRenderer(float def) {
         return -1;
     }
 
+    @ModifyVariable(method = "renderStatusBars", at = @At(value = "STORE", ordinal = 0))
+    private float modifyMaxHealth(float float_1) {
+        return Math.min(20.0F, float_1);
+    }
+
+    @ModifyVariable(method = "renderStatusBars", at = @At(value = "STORE"), ordinal = 6)
+    private int modifyAbsorption(int int_7) {
+        return Math.min(20, int_7);
+    }
+
     @Inject(method = "renderStatusBars", locals = LocalCapture.CAPTURE_FAILEXCEPTION,
             at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V", args = "ldc=health"))
     private void render(CallbackInfo info, PlayerEntity player) {
-        InGameHud hud = (InGameHud) (Object) this;
-        int currentHealth = MathHelper.ceil(player.getHealth());
-        boolean highlight = this.field_2032 > (long) this.ticks && (this.field_2032 - (long) this.ticks) / 3L % 2L == 1L;
-
-        int left = this.scaledWidth / 2 - 91;
-        int top = this.scaledHeight - 39;
-        float maxHealth = player.getHealthMaximum();
-        int currentAbsorption = MathHelper.ceil(player.getAbsorptionAmount());
-
-        int regenerationTicks = -1;
-        if (player.hasPotionEffect(StatusEffects.REGENERATION)) {
-            regenerationTicks = this.ticks % MathHelper.ceil(maxHealth + 5.0F);
-        }
-
-        // Health Renderer
-        int maxHealthIterator = MathHelper.ceil(maxHealth / 2.0f);
-        for (int i = 1; i <= maxHealthIterator; i++) {
-            int x = left + (i - 1) % 10 * 8;
-            int y = top;
-
-            if (currentHealth <= 4 && maxHealth > 4) {
-                y += random.nextInt(2);
-            }
-
-            if (i == regenerationTicks) {
-                y -= 2;
-            }
-
-            int effect_offset = 16 + (player.hasPotionEffect(StatusEffects.POISON) ? 36 : player.hasPotionEffect(StatusEffects.WITHER) ? 72 : 0);
-            int hardcore_offset = player.world.getLevelProperties().isHardcore() ? 5 : 0;
-            int highlight_offset = highlight ? 9 : 0;
-            int iterHealth = i * 2 - 1;
-
-            if (iterHealth <= 20) {
-                if (maxHealth < 19 && i == maxHealthIterator) {
-                    this.client.getTextureManager().bindTexture(HEART_TEXTURES);
-                    for (int z = 1; z <= (20 - (int) maxHealth) / 2; z++) {
-                        int xz = left + (MathHelper.ceil(maxHealth / 2.0f) + z - 1) % 10 * 8;
-
-                        hud.drawTexturedRect(xz, y, 0, 0, 9, 9);
-                    }
-                    this.client.getTextureManager().bindTexture(Drawable.ICONS);
-                }
-
-                if (i * 2 - 1 > maxHealth - 1) {
-                    this.client.getTextureManager().bindTexture(HEART_TEXTURES);
-                    hud.drawTexturedRect(x, y, 9 + highlight_offset, 0, 9, 9);
-                    this.client.getTextureManager().bindTexture(Drawable.ICONS);
-                } else {
-                    hud.drawTexturedRect(x, y, 16 + highlight_offset, 0, 9, 9);
-                }
-            }
-
-            if (highlight) {
-                if (i * 2 - 1 < currentHealth) {
-                    hud.drawTexturedRect(x, y, effect_offset + 54, 9 * hardcore_offset, 9, 9);
-                }
-
-                if (i * 2 - 1 == currentHealth) {
-                    hud.drawTexturedRect(x, y, effect_offset + 63, 9 * hardcore_offset, 9, 9);
-                }
-            }
-
-
-            if (iterHealth <= 20) {
-                if (iterHealth < currentHealth) {
-                    hud.drawTexturedRect(x, y, effect_offset + 36, 9 * hardcore_offset, 9, 9);
-                }
-
-                if (iterHealth == currentHealth) {
-                    hud.drawTexturedRect(x, y, effect_offset + 45, 9 * hardcore_offset, 9, 9);
-                }
-            } else {
-                this.client.getTextureManager().bindTexture(HEART_TEXTURES);
-                effect_offset = player.hasPotionEffect(StatusEffects.POISON) ? 9 : 0;
-                hardcore_offset = player.world.getLevelProperties().isHardcore() ? 27 : 0;
-
-                int type_offset = ((iterHealth - 20) / 20) % 12;
-
-                if (iterHealth < currentHealth) {
-                    hud.drawTexturedRect(x, y, effect_offset + 15 * type_offset, hardcore_offset + effect_offset + 10, 9, 9);
-                }
-
-                if (iterHealth == currentHealth) {
-                    hud.drawTexturedRect(x, y, effect_offset + 9 + 15 * type_offset, hardcore_offset + effect_offset + 10, 5, 9);
-                }
-
-                this.client.getTextureManager().bindTexture(Drawable.ICONS);
-            }
-        }
-
-        // Absorption Renderer
-        int maxAbsorptionIterator = MathHelper.ceil(currentAbsorption / 2.0f);
-        for (int i = 1; i <= maxAbsorptionIterator; i++) {
-            int x = left + (i - 1) % 10 * 8;
-            int y = top - 10;
-
-            /*
-            if (currentHealth <= 4 && maxHealth > 4) {
-                y += random.nextInt(2);
-            }
-            */
-
-            if (i == regenerationTicks) {
-                y -= 2;
-            }
-
-            int effect_offset = 16 + (player.hasPotionEffect(StatusEffects.POISON) ? 36 : player.hasPotionEffect(StatusEffects.WITHER) ? 72 : 0);
-            int hardcore_offset = player.world.getLevelProperties().isHardcore() ? 5 : 0;
-            int highlight_offset = highlight ? 1 : 0;
-            int iterAbsorption = i * 2 - 1;
-
-            if (iterAbsorption > currentAbsorption - 1) {
-                this.client.getTextureManager().bindTexture(HEART_TEXTURES);
-                hud.drawTexturedRect(x, y, 9 + highlight_offset * 9, 0, 9, 9);
-                this.client.getTextureManager().bindTexture(Drawable.ICONS);
-            } else {
-                hud.drawTexturedRect(x, y, 16 + highlight_offset * 9, 0, 9, 9);
-            }
-
-            if (iterAbsorption <= 20 || player.hasPotionEffect(StatusEffects.WITHER) ) {
-
-                if (iterAbsorption == currentAbsorption && currentAbsorption % 2 == 1) {
-                    hud.drawTexturedRect(x, y, effect_offset + 153, 9 * hardcore_offset, 9, 9);
-                } else {
-                    hud.drawTexturedRect(x, y, effect_offset + 144, 9 * hardcore_offset, 9, 9);
-                }
-            } else {
-                this.client.getTextureManager().bindTexture(HEART_TEXTURES);
-                effect_offset = player.hasPotionEffect(StatusEffects.POISON) ? 9 : 0;
-                hardcore_offset = player.world.getLevelProperties().isHardcore() ? 27 : 0;
-
-                int type_offset = ((iterAbsorption - 20) / 20) % 12;
-
-                if (iterAbsorption == currentAbsorption && currentAbsorption % 2 == 1) {
-                    hud.drawTexturedRect(x, y, effect_offset + 9 + 15 * type_offset, hardcore_offset + effect_offset + 75, 5, 9);
-                } else {
-                    hud.drawTexturedRect(x, y, effect_offset + 15 * type_offset, hardcore_offset + effect_offset + 75, 9, 9);
-                }
-                this.client.getTextureManager().bindTexture(Drawable.ICONS);
-            }
-        }
+        HealthRenderer.INSTANCE.render(player);
     }
 }
