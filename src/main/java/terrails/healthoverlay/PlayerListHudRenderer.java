9294f83c3121fb326943aab7b0cfe504c6a05728
Terrails <terrails@outlook.com>
2020-07-17 12:00:31 +0200

    Mixin not working outside dev #4 I was just stupid and didn't use Overwrite, while I was at it I just converted it to an Inject that basically overwrites the if block

--- src/main/java/terrails/healthoverlay/PlayerListHudRenderer.java
+++ src/main/java/terrails/healthoverlay/PlayerListHudRenderer.java
@@ -32,85 +32,69 @@ public class PlayerListHudRenderer {
 
     public void render(ScoreboardObjective scoreboardObjective, int y, String string, int x, int k, PlayerListEntry playerListEntry, MatrixStack matrixStack, long showTime) {
         int scoreValue = scoreboardObjective.getScoreboard().getPlayerScore(string, scoreboardObjective).getScore();
-        if (scoreboardObjective.getRenderType() == ScoreboardCriterion.RenderType.HEARTS) {
 
-            this.client.getTextureManager().bindTexture(DrawableHelper.GUI_ICONS_TEXTURE);
-            long m = Util.getMeasuringTimeMs();
-            if (showTime == playerListEntry.method_2976()) {
-                if (scoreValue < playerListEntry.method_2973()) {
-                    playerListEntry.method_2978(m);
-                    playerListEntry.method_2975((this.inGameHud.getTicks() + 20));
-                } else if (scoreValue > playerListEntry.method_2973()) {
-                    playerListEntry.method_2978(m);
-                    playerListEntry.method_2975((this.inGameHud.getTicks() + 10));
-                }
-            }
+        // Make sure to only render the first 10 vanilla hearts
+        scoreValue = Math.min(scoreValue, 20);
 
-            if (m - playerListEntry.method_2974() > 1000L || showTime != playerListEntry.method_2976()) {
-                playerListEntry.method_2972(scoreValue);
-                playerListEntry.method_2965(scoreValue);
+        this.client.getTextureManager().bindTexture(DrawableHelper.GUI_ICONS_TEXTURE);
+        long m = Util.getMeasuringTimeMs();
+        if (showTime == playerListEntry.method_2976()) {
+            if (scoreValue < playerListEntry.method_2973()) {
                 playerListEntry.method_2978(m);
+                playerListEntry.method_2975((this.inGameHud.getTicks() + 20));
+            } else if (scoreValue > playerListEntry.method_2973()) {
+                playerListEntry.method_2978(m);
+                playerListEntry.method_2975((this.inGameHud.getTicks() + 10));
             }
-
-            playerListEntry.method_2964(showTime);
-            playerListEntry.method_2972(scoreValue);
-
-            // Make sure to only render the first 10 vanilla hearts
-            scoreValue = Math.min(scoreValue, 20);
-            int method_2960 = Math.min(playerListEntry.method_2960(), 20);
-
-            int n = MathHelper.ceil((float)Math.max(scoreValue, method_2960) / 2.0F);
-            int o = Math.max(MathHelper.ceil((float)(scoreValue / 2)), Math.max(MathHelper.ceil((float)(method_2960 / 2)), 10));
-            boolean highlight = playerListEntry.method_2961() > (long)this.inGameHud.getTicks() && (playerListEntry.method_2961() - (long)this.inGameHud.getTicks()) / 3L % 2L == 1L;
-            if (n > 0) {
-                int p = MathHelper.floor(Math.min((float)(k - x - 4) / (float)o, 9.0F));
-                if (p > 3) {
-                    int r;
-                    for(r = n; r < o; ++r) {
-                        this.dh.drawTexture(matrixStack, x + r * p, y, highlight ? 25 : 16, 0, 9, 9);
-                    }
-
-                    for(r = 0; r < n; ++r) {
-                        this.dh.drawTexture(matrixStack, x + r * p, y, highlight ? 25 : 16, 0, 9, 9);
-                        if (highlight) {
-                            if (r * 2 + 1 < method_2960) {
-                                this.dh.drawTexture(matrixStack, x + r * p, y, 70, 0, 9, 9);
-                            }
-
-                            if (r * 2 + 1 == method_2960) {
-                                this.dh.drawTexture(matrixStack, x + r * p, y, 79, 0, 9, 9);
-                            }
-                        }
-
-                        if (r * 2 + 1 < scoreValue) {
-                            this.dh.drawTexture(matrixStack, x + r * p, y, r >= 10 ? 160 : 52, 0, 9, 9);
-                        }
-
-                        if (r * 2 + 1 == scoreValue) {
-                            this.dh.drawTexture(matrixStack, x + r * p, y, r >= 10 ? 169 : 61, 0, 9, 9);
-                        }
-                    }
-
-                    // Render additional hearts
-                    scoreValue = scoreboardObjective.getScoreboard().getPlayerScore(string, scoreboardObjective).getScore();
-                    renderHearts(matrixStack, scoreValue, x, y, p);
-
-                } else {
-                    float f = MathHelper.clamp((float)scoreValue / 20.0F, 0.0F, 1.0F);
-                    int s = (int)((1.0F - f) * 255.0F) << 16 | (int)(f * 255.0F) << 8;
-                    String string2 = "" + (float)scoreValue / 2.0F;
-                    if (k - this.client.textRenderer.getWidth(string2 + "hp") >= x) {
-                        string2 = string2 + "hp";
-                    }
-
-                    this.client.textRenderer.drawWithShadow(matrixStack, string2, (float)((k + x) / 2 - this.client.textRenderer.getWidth(string2) / 2), (float)y, s);
-                }
-            }
-        } else {
-            String string3 = Formatting.YELLOW + "" + scoreValue;
-            this.client.textRenderer.drawWithShadow(matrixStack, string3, (float)(k - this.client.textRenderer.getWidth(string3)), (float)y, 16777215);
         }
 
+        if (m - playerListEntry.method_2974() > 1000L || showTime != playerListEntry.method_2976()) {
+            playerListEntry.method_2972(scoreValue);
+            playerListEntry.method_2965(scoreValue);
+            playerListEntry.method_2978(m);
+        }
+
+        playerListEntry.method_2964(showTime);
+        playerListEntry.method_2972(scoreValue);
+
+        // Also limit this
+        int method_2960 = Math.min(playerListEntry.method_2960(), 20);
+
+        int n = MathHelper.ceil((float) Math.max(scoreValue, method_2960) / 2.0F);
+        int o = Math.max(MathHelper.ceil((float) (scoreValue / 2)), Math.max(MathHelper.ceil((float) (method_2960 / 2)), 10));
+        boolean highlight = playerListEntry.method_2961() > (long) this.inGameHud.getTicks() && (playerListEntry.method_2961() - (long) this.inGameHud.getTicks()) / 3L % 2L == 1L;
+        if (n > 0) {
+            int p = MathHelper.floor(Math.min((float) (k - x - 4) / (float) o, 9.0F));
+            int r;
+            for (r = n; r < o; ++r) {
+                this.dh.drawTexture(matrixStack, x + r * p, y, highlight ? 25 : 16, 0, 9, 9);
+            }
+
+            for (r = 0; r < n; ++r) {
+                this.dh.drawTexture(matrixStack, x + r * p, y, highlight ? 25 : 16, 0, 9, 9);
+                if (highlight) {
+                    if (r * 2 + 1 < method_2960) {
+                        this.dh.drawTexture(matrixStack, x + r * p, y, 70, 0, 9, 9);
+                    }
+
+                    if (r * 2 + 1 == method_2960) {
+                        this.dh.drawTexture(matrixStack, x + r * p, y, 79, 0, 9, 9);
+                    }
+                }
+
+                if (r * 2 + 1 < scoreValue) {
+                    this.dh.drawTexture(matrixStack, x + r * p, y, r >= 10 ? 160 : 52, 0, 9, 9);
+                }
+
+                if (r * 2 + 1 == scoreValue) {
+                    this.dh.drawTexture(matrixStack, x + r * p, y, r >= 10 ? 169 : 61, 0, 9, 9);
+                }
+            }
+
+            // Render additional hearts
+            scoreValue = scoreboardObjective.getScoreboard().getPlayerScore(string, scoreboardObjective).getScore();
+            renderHearts(matrixStack, scoreValue, x, y, p);
+        }
     }
 
     private void renderHearts(MatrixStack matrixStack, int score, int xPosition, int yPos, int p) {
--- src/main/java/terrails/healthoverlay/mixin/PlayerListHudMixin.java
+++ src/main/java/terrails/healthoverlay/mixin/PlayerListHudMixin.java
@@ -7,9 +7,10 @@ import net.minecraft.client.gui.hud.PlayerListHud;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.scoreboard.ScoreboardObjective;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import terrails.healthoverlay.PlayerListHudRenderer;
 
 @Mixin(PlayerListHud.class)
@@ -19,10 +20,13 @@ public abstract class PlayerListHudMixin extends DrawableHelper {
     @Shadow private @Final InGameHud inGameHud;
     @Shadow private long showTime;
 
-    private void renderScoreboardObjective(ScoreboardObjective scoreboardObjective, int y, String string, int x, int k, PlayerListEntry playerListEntry, MatrixStack matrixStack) {
+    @Inject(method = "renderScoreboardObjective", cancellable = true, at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/client/MinecraftClient;getTextureManager()Lnet/minecraft/client/texture/TextureManager;", shift = At.Shift.BEFORE))
+    private void renderScoreboardObjective(ScoreboardObjective scoreboardObjective, int y, String string, int x, int k, PlayerListEntry playerListEntry, MatrixStack matrixStack, CallbackInfo callbackInfo) {
         if (PlayerListHudRenderer.INSTANCE == null) {
             PlayerListHudRenderer.INSTANCE = new PlayerListHudRenderer(this.client, this.inGameHud, this);
         }
         PlayerListHudRenderer.INSTANCE.render(scoreboardObjective, y, string, x, k, playerListEntry, matrixStack, showTime);
+        callbackInfo.cancel();
     }
 }
